13a8b25478eef2c11af7e9d80009a14b572b54ff
nYSaaa <voranysa@gmail.com>
2024-11-23 14:35:44 -0600

    Add files via upload

---
+++ hw2.py
@@ -0,0 +1,39 @@
+book1 =  {
+    "Name" : "Love and Gelato" , 
+    "Author" : "Jenna Evans Welch" ,
+    "Genre" : "Mystery/Romance" ,
+    "First Published" : "2016" ,
+}
+print(book1)
+
+book2 = {
+    "Name" : "All Fall Down",
+    "Author" : "Ally Carter" ,
+    "Genre" : "Mystery/Adventure" ,
+    "First Published" : "2015" ,
+}
+print(book2)
+
+book3 = {
+    "Name" : "The Bethroted" ,
+    "Author" : "Kiera Cass" ,
+    "Genre" : "Fantasy/Romance" ,
+    "First Published" : "2020" ,
+}
+print(book3)
+
+book4 = {
+    "Name" : "The Mother Daugther Book Club" ,
+    "Author" : "Heather Vogel Fredrick" ,
+    "Genre" : "Realistic" ,
+    "First Published" : "2007" ,
+}
+print(book4)
+
+book5 = {
+    "Name" : "The Hunger Games" , 
+    "Author" : "Suzzane Collins" , 
+    "Genre" : "Dystopian/Romance" ,
+    "First Published" : "2008" ,
+}
+print(book5)
